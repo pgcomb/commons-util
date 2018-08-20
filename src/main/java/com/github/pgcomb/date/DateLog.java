a5fa1c49f4561b4cbcbb50ade8c85daf7d9a0da1
wangdongxu <wangdongxu@wondertek.com.cn>
2018-08-20 21:34:24 +0800

    日期记录

--- src/main/java/com/github/pgcomb/date/DateLog.java
+++ src/main/java/com/github/pgcomb/date/DateLog.java
@@ -2,27 +2,43 @@ package com.github.pgcomb.date;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-public abstract class DateLog {
+public class DateLog {
 
-    private static final  ThreadLocal<WindowLink<LocalDateTime>> DATES = new ThreadLocal<>();
+    private static final String DEF_NAME = "_def";
+    public DateLog() {
+    }
 
-    private static void init() {
-        DATES.set(new WindowLink<>(2, LocalDateTime.now()));
+    private static final  ThreadLocal<Map<String,WindowLink<LocalDateTime>>> DATES = new ThreadLocal<>();
+
+    private static void init(String name) {
+        if (DATES.get() == null){
+            Map<String,WindowLink<LocalDateTime>> objects = new HashMap<>(1);
+            DATES.set(objects);
+        }
+        DATES.get().put(name,new WindowLink<>(2,LocalDateTime.now()));
     }
 
     public static void record(){
-        if (DATES.get() == null){
-            init();
-        }
-        DATES.get().add(LocalDateTime.now());
+        record(DEF_NAME);
     }
 
-    public static void record(ThreeFunc<LocalDateTime,LocalDateTime,Duration> threeFunc){
-        if (DATES.get() == null){
-            init();
+    public static void record(String name){
+        if (DATES.get() == null || DATES.get().get(name) == null){
+            init(name);
         }
-        WindowLink<LocalDateTime> localDateTimes = DATES.get();
+        DATES.get().get(name).add(LocalDateTime.now());
+    }
+    public static void record(ThreeFunc<LocalDateTime,LocalDateTime,Duration> threeFunc){
+        record(DEF_NAME,threeFunc);
+    }
+    public static void record(String name, ThreeFunc<LocalDateTime,LocalDateTime,Duration> threeFunc){
+        if (DATES.get() == null || DATES.get().get(name) == null){
+            init(name);
+        }
+        WindowLink<LocalDateTime> localDateTimes = DATES.get().get(name);
         localDateTimes.add(LocalDateTime.now());
         LocalDateTime dateTime0 = localDateTimes.get(0);
         LocalDateTime dateTime1 = localDateTimes.get(1);
