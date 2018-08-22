ac436fa926e0a79d594285b5c72910dfd38e7e0b
wangdongxu <wangdongxu@wondertek.com.cn>
2018-08-22 13:26:37 +0800

    配置

---
+++ src/main/java/com/github/pgcomb/course/KeyEnum.java
@@ -0,0 +1,25 @@
+package com.github.pgcomb.course;
+
+/**
+ * Title: KeyEnum <br>
+ * Description: KeyEnum <br>
+ * Date: 2018年08月15日
+ *
+ * @author 王东旭
+ * @version 1.0.0
+ * @since jdk8
+ */
+public interface KeyEnum {
+
+    /**
+     * 获取key
+     * @return key
+     */
+    String getKey();
+
+    /**
+     * 默认值
+     * @return 默认值
+     */
+    String getDef();
+}
